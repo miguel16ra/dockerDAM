8582f6eedc751b156afa1670632f6687a2a56f95
miguel16ra <miguel16raa@gmail.com>
2026-04-24 12:27:50 +0200

    Add SQL script to create users and set privileges

---
+++ sql/users.sql
@@ -0,0 +1,7 @@
+CREATE USER 'admin_prometeo'@'%' IDENTIFIED BY 'Admin123';
+GRANT ALL PRIVILEGES ON prometeo.* TO 'admin_prometeo'@'%';
+
+CREATE USER 'lector'@'%' IDENTIFIED BY 'Read123';
+GRANT SELECT ON prometeo.* TO 'lector'@'%';
+
+FLUSH PRIVILEGES;
